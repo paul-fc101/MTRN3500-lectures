68318e5885afee58c8eb8a7ad2dd1903ee37c737
Paul <paul.fc101@gmail.com>
2024-09-23 11:55:58 +1000

    Changes for lectures

--- Week 02/BasicClrFeatures/main.cpp
+++ Week 02/BasicClrFeatures/main.cpp
@@ -7,16 +7,17 @@ int main()
 	int i = 50;
 	double a = 3.14159;
 	// Printing to screen
-	Console::WriteLine("Execution Completed!");
-	//// Color control
+	// Console::Write("Execution Completed!"); // print something to the terminal with NO new line after
+	Console::WriteLine("Execution Completed!"); // print something to the terminal with a new line after
+	// Color control
 	Console::ForegroundColor = ConsoleColor::Red;
 	// Formatted printing
 	do
 	{
-		Console::SetCursorPosition(10, 10);
+		Console::SetCursorPosition(10, 10); // keeps the output in the same position on the terminal
 		Console::WriteLine("{0, 12:D}              {1, 12:F3}", i++, a);
 	} while (!Console::KeyAvailable);
-	Console::ForegroundColor = ConsoleColor::White;
+	Console::ForegroundColor = ConsoleColor::White; // set colour back to normal (good practice)
 
 	// wait for a keypress
 	Console::ReadKey();
